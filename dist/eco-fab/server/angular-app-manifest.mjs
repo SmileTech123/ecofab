3570d91eb88ec671edf5d9876c2e7c8eb86a079a
smiletech123 <fabiogermana01@gmail.com>
2025-02-03 13:58:56 +0100

    latest

--- dist/eco-fab/server/angular-app-manifest.mjs
+++ dist/eco-fab/server/angular-app-manifest.mjs
@@ -14,8 +14,8 @@ export default {
   ]
 },
   assets: {
-    'index.csr.html': {size: 26509, hash: '1969075e819e6690c2682db6e78b845840a07ad1a8a473a15b285a2625694c54', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17239, hash: '4dda14049624350bdebed501d451536fd04aac0891c59c1f25db2499739c882d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-N35DQA47.css': {size: 9874, hash: 'ZbzMYIoqyyg', text: () => import('./assets-chunks/styles-N35DQA47_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 26503, hash: '7bd6037fa15f6b72e54bbcf2ca864eb1232bd6a697360976cd660ba1c3181bae', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17239, hash: 'b970060b40a18ecdb175c8496d38b6103a7d4a913f351ce8083a1c7b9b6bd7de', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-TJNHR34F.css': {size: 9868, hash: 'ZJHoSGaLOtE', text: () => import('./assets-chunks/styles-TJNHR34F_css.mjs').then(m => m.default)}
   },
 };
